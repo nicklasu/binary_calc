7284ed2a9806a515690f42f8366649f3708160b0
unknown <meteg66@gmail.com>
2020-12-15 13:29:49 +0200

    Modified function to handle bigger binary values

--- src/Functions/GetNumber.js
+++ src/Functions/GetNumber.js
@@ -1,21 +1,13 @@
-function getNumber(number) {
-  if (number === "0000") return 0;
-  if (number === "0001") return 1;
-  if (number === "0010") return 2;
-  if (number === "0011") return 3;
-  if (number === "0100") return 4;
-  if (number === "0101") return 5;
-  if (number === "0110") return 6;
-  if (number === "0111") return 7;
-  if (number === "1000") return 8;
-  if (number === "1001") return 9;
-  if (number === "1010") return 10;
-  if (number === "1011") return 11;
-  if (number === "1100") return 12;
-  if (number === "1101") return 13;
-  if (number === "1110") return 14;
-  if (number === "1111") return 15;
-
-  return;
+/**
+ * Convert binary string to number and return it
+ * If string is not valid binary return 0
+ * @example 0011 => 3
+ * @example 0022 => 0
+ * @param {String} binary
+ * @returns {Number}
+ */
+function getNumber(binary) {
+  const number = parseInt(binary, 2);
+  return number ? number : 0;
 }
 export default getNumber;
