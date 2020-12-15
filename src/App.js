325cbf1bfead353424824d0f742fe9761c81231e
unknown <meteg66@gmail.com>
2020-12-15 11:22:19 +0200

    Eslint to gitignore

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 function App() {
-  return <p>testi</p>;
+  const [num1, setNum1] = useState();
+  const [num2, setNum2] = useState();
+
+  console.log(Number(num1) + Number(num2));
+
+  return (
+    <>
+      <input onChange={(e) => setNum1(e.target.value)}></input>
+      <input onChange={(e) => setNum2(e.target.value)}></input>
+    </>
+  );
 }
 
 export default App;
