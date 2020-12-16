2f5319cd9aed00e0c8a3e6e52b40f91c34e30b65
unknown <meteg66@gmail.com>
2020-12-16 19:55:40 +0200

    Split string to array

--- src/Functions/BinaryToArray.js
+++ src/Functions/BinaryToArray.js
@@ -1,12 +1,12 @@
 /**
- * Divides string to array and reverses it
- * @example 'heippa' => ['a','p','p','i','e','h']
- * @example '000111' => ['1','1','1','0','0','0']
+ * Divides string to array
+ * @example 'heippa' => ['h','e','i','p','p','a']
+ * @example '000111' => ['0','0','0','1','1','1']
  * @param {String} binary
  * @returns {Array}
  */
 function binaryToArray(binary) {
-  return binary.split("").reverse();
+  return binary.split("");
 }
 
 export default binaryToArray;
