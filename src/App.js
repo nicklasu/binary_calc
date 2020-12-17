4dc815133baf9792b37e0f12eeab4cfb96632158
unknown <meteg66@gmail.com>
2020-12-17 11:29:36 +0200

    Added CalcSteps component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,17 @@ import { useState } from "react";
 import "./App.css";
 import Input from "./Components/Input/Input";
 import Result from "./Components/Result/Result";
+import CalcSteps from "./Components/CalcSteps/CalcSteps";
+import getNumber from "./Functions/GetNumber";
 function App() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
-
-  // Poistin ne funktiot
-  // Ei niissä mitään vikaa ollut hyvin ne toimi
-  // Sitten kun saat tämän toimimaan niin huomaat että
-  // Tämä tyyli on yksinkertaisempi
-
-  // Buttonit josta toinen vaihtaisi -> calc <- muuttujan arvoksi (+)
-  // ja toinen vaihtaisi (-)
-  // setCalc liittyy vahvasti siihen
-  // Löytyy Reactin sivulta kun googlettaa useState
-
-  // Ja vois koittaa tehdä aina pull requestin ennen ku pushataan
-  // Ehtisin kattoa sitä koodia ja kommentoida ennen ku liitetään se projektiin
-  // En ole varma itsekään miten se tarkalleen tehdään mutta se on oikea tyyli tehä näitä :D
-
-  //Jätin oletuksena tän + stateen koska silloin ruudulle ilmestyy interaktiivisesti lukuja, muuten vasta kun painaa nappeja.
   const [calc, setCalc] = useState("+");
+
+  // TODO:
+  // Vois jotenkin tyylitellä kevyesti tätä sivustoa
+  // Kissakuvia ja mageita videoita
+  // Muista golden ratio
 
   return (
     <main className="main-content">
@@ -29,7 +20,11 @@ function App() {
       <Input setNumber={setNum2} /> <br />
       <button onClick={() => setCalc("-")}>-</button>
       <button onClick={() => setCalc("+")}>+</button>
-      <Result num1={num1} num2={num2} calc={calc} />
+      <Result num1={getNumber(num1)} num2={getNumber(num2)} calc={calc} />
+      {
+        // Show CalcSteps after user has submitted in both fields
+        num1 && num2 ? <CalcSteps num1={num1} num2={num2} calc={calc} /> : null
+      }
     </main>
   );
 }
