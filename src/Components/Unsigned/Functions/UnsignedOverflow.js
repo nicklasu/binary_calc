98cf50604907349dab4b952ab11eb9a2ceee6784
unknown <meteg66@gmail.com>
2020-12-18 16:17:01 +0200

    Returns correct result now

--- src/Components/Unsigned/Functions/UnsignedOverflow.js
+++ src/Components/Unsigned/Functions/UnsignedOverflow.js
@@ -1,8 +1,10 @@
+import addZeros from "./addZeros";
 // Check if result is bigger than bit size
 // Or smaller than 0
+// Add zeros according to bit size
 function unsignedOverflow(binary, bitSize) {
-  let overflow = binary.length > bitSize || parseInt(binary, 2) < 0;
-  return overflow ? "overflow!" : binary;
+  const overflow = binary.length > bitSize || parseInt(binary, 2) < 0;
+  return overflow ? "overflow!" : addZeros(binary, bitSize);
 }
 
 export default unsignedOverflow;
