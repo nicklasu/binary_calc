ad772cd2416a4bb22470a24cc63d41993a5b805f
nicklasu <nicklass84@gmail.com>
2020-12-18 18:48:18 +0200

    Ongelma saada signed vai unsigned näkyviin

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Input from "./Components/Input/Input";
 import Result from "./Components/Result/Result";
 import Unsigned from "./Components/Unsigned/Unsigned";
 import getNumber from "./Functions/GetNumber";
+import Header from "./Components/Header/Header";
+import UnsignedOr from "./Functions/UnsignedOr";
 function App() {
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
   const [calc, setCalc] = useState("+");
+  const [sign, setSign] = useState("unsigned");
 
   // TODO:
   // Vois jotenkin tyylitellä kevyesti tätä sivustoa
@@ -16,11 +19,15 @@ function App() {
 
   return (
     <main className="main-content">
+      <Header />
       <Input setNumber={setNum1} />
       <Input setNumber={setNum2} /> <br />
       <button onClick={() => setCalc("-")}>-</button>
-      <button onClick={() => setCalc("+")}>+</button>
-      <Result num1={getNumber(num1)} num2={getNumber(num2)} calc={calc} />
+      <button onClick={() => setCalc("+")}>+</button> <br />
+      <button onClick={() => setSign("signed")}>signed</button>
+      <button onClick={() => setSign("unsigned")}>unsgnd</button>
+      <Result num1={getNumber(num1)} num2={getNumber(num2)} />
+      <UnsignedOr sign={sign} />
       {
         // Show Unsigned after user has submitted in both fields
         num1 && num2 ? <Unsigned num1={num1} num2={num2} calc={calc} /> : null
---
+++ src/Functions/UnsignedOr.js
@@ -0,0 +1,18 @@
+function UnsignedOr(sign) {
+  if (sign === "signed") {
+    return (
+      <section>
+        <h1>signed</h1>
+      </section>
+    );
+  }
+  if (sign === "unsigned") {
+    return (
+      <section>
+        <h1>signed</h1>
+      </section>
+    );
+  }
+}
+
+export default UnsignedOr;
