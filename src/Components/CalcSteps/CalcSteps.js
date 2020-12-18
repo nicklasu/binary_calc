d77600a31721ea5ebcee89e7a49de70639f1aad5
unknown <meteg66@gmail.com>
2020-12-18 12:59:13 +0200

    Slimmed down, still needs more clarity

--- src/Components/CalcSteps/CalcSteps.js
+++ src/Components/CalcSteps/CalcSteps.js
@@ -6,6 +6,16 @@ import calculate from "../../Functions/Calculate";
 
 // Tätä pitää muuttaa vielä jotenkin selvemmäksi
 function CalcSteps({ num1, num2, calc }) {
+  // Total +/- as integer and binary
+  let totalInt = calculate(getNumber(num1), getNumber(num2), calc);
+  let totalBin = totalInt.toString(2);
+
+  // If result is negative
+  // Remove "-" from totalBin
+  if (totalInt < 0) totalBin = totalBin.substring(1);
+
+  // Set binaries as arrays
+  // Add zeros if needed
   let binary1 = addMissingZeros(
     binaryToArray(num1),
     binaryToArray(num2).length
@@ -14,22 +24,20 @@ function CalcSteps({ num1, num2, calc }) {
     binaryToArray(num2),
     binaryToArray(num1).length
   );
-  // Total +/- in binary
 
-  let total = calculate(getNumber(num1), getNumber(num2), calc).toString(2);
+  // Total binary as array
+  // Add zeros if needed
+  totalBin = binaryToArray(totalBin);
+  totalBin = addMissingZeros(
+    totalBin,
+    Math.max(binary1.length, binary2.length)
+  );
 
-  //Check if the user has chosen subtraction, if they have, perform these steps:
-  if (calc === "-") {
-    //Make total into a list and compare it to num1 to get the right length
-    var sum = addMissingZeros(binaryToArray(total), binaryToArray(num1).length);
-    //If user has chosen other option than subtraction then just take total and make it to sum so it can be represented in the html.
-  } else {
-    sum = total;
-  }
-  const overflow =
-    total.toString(2).length > binary1.length ||
-    total.toString(2).length > binary2.length ||
-    sum < [0, 0, 0, 0];
+  // Check if result is overflowing
+  let overflow =
+    totalBin.length > binary1.length ||
+    totalBin.length > binary2.length ||
+    totalInt < 0;
 
   return (
     <section className="section-calc">
@@ -38,11 +46,7 @@ function CalcSteps({ num1, num2, calc }) {
       <hr />
       {
         // Display overflow message if it overflows(!?)
-        overflow ? (
-          <p>overflow!</p>
-        ) : (
-          <p style={{ textAlign: "right" }}>{sum}</p>
-        )
+        overflow ? <p>overflow!</p> : <p>{totalBin}</p>
       }
     </section>
   );
