c27edd3e9abbca97fcf80ab8237bf78ccf3be84d
unknown <meteg66@gmail.com>
2020-12-21 07:57:41 +0200

    This file needs fixing

--- src/Components/Unsigned/Unsigned.js
+++ src/Components/Unsigned/Unsigned.js
@@ -3,17 +3,36 @@ import getNumber from "../../Functions/GetNumber";
 import calculate from "../../Functions/Calculate";
 import addZeros from "./Functions/addZeros";
 import checkOverflow from "./Functions/UnsignedOverflow";
-import { waitFor } from "@testing-library/react";
+import { useEffect } from "react";
+
+// TODO:
+// Niin ku tän komponentin nimi kertoo, se näyttää vaan Unsigned version
+// Tän komponentin sisälle ei muuta kamaa, eli tän komponentin
+// Tehtävä on tietää vaan niistä asioista mitä sen nimi kertoo
+
+// Jos tämän ja Signed komponentin välillä vaihdellaan näkymää
+// Niin tehdään se tuolla UnSignedOr komponentissa
+// Eli UnSignedOr pitäisi sisällään 2 komponenttia (Signed, UnSigned)
+// UnSignedOrn sisälle se logiikka mikä vaihtaa näkymää niiden kahden komponentin välillä
+// if (sign === "unsigned") niin näytä Unsigned komponentti
+// Vähän samalla idealla kuin mitä oot tehnyt tonne Header komponenttiin
+
+// Reactissa on hyvä pitää komponentit mahdollisimman pieninä, ainakin tän tason ohjelmassa
+// Helpompi testata niitä ja tehdä muutoksia, kun komponentissa on mahdollisimman vähän koodia tai
+// Siihen kohdistuu mahdollisimman vähän sivuvaikutuksia
 
 // Display unsigned char calculation
-function Unsigned({ num1, num2, calc, sign }) {
-  // Set max bit size
-  // Add zeros if needed
-  // Display result as binary
+function Unsigned({ num1, num2, setNum1, setNum2, calc, sign }) {
+  // Set max bit size from user input
+  const bitSize = Math.max(num1.length, num2.length);
+
+  // When component updates, adds needed zeros according to bitSize
+  useEffect(() => {
+    setNum1(addZeros(num1, bitSize));
+    setNum2(addZeros(num2, bitSize));
+  });
   if (sign === "unsigned") {
-    const bitSize = Math.max(num1.length, num2.length);
-    num1 = addZeros(num1, bitSize);
-    num2 = addZeros(num2, bitSize);
+    // Display result as binary
     let resultAsBinary = calculate(
       getNumber(num1),
       getNumber(num2),
