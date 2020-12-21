fd584b59326fd8109b23f298aa00d73118ee1e53
unknown <meteg66@gmail.com>
2020-12-21 14:07:14 +0200

    Folder for testing

---
+++ src/__tests__/CheckOverFlow.test.js
@@ -0,0 +1,24 @@
+import checkOverflow from "../Components/Unsigned/Functions/UnsignedOverflow";
+
+// Show results
+describe("Display result as binary on Unsigned component", () => {
+  test("Show binary if not overflowing", () => {
+    expect(checkOverflow("1111", 4)).toBe("1111");
+  });
+  test("Show 000 if result is 0 and bit size is 3", () => {
+    expect(checkOverflow("000", 3)).toBe("000");
+  });
+  test("Show 4 digit binary when bit size is 4", () => {
+    expect(checkOverflow("01", 4)).toBe("0001");
+  });
+});
+
+// Check overflow
+describe("Display message: overflow!", () => {
+  test("Show overflow if it exceeds bit size", () => {
+    expect(checkOverflow("11111", 4)).toBe("overflow!");
+  });
+  test("Show overflow if binary is negative", () => {
+    expect(checkOverflow("-1", 4)).toBe("overflow!");
+  });
+});
