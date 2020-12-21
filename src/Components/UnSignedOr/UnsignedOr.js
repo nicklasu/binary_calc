5d629cc2ec02241c721e01fb1fa5ff5db2599d22
nicklasu <nicklass84@gmail.com>
2020-12-21 13:39:36 +0200

    UnsignedOr shows now in components rather than functions

--- src/Components/UnSignedOr/UnsignedOr.js
+++ src/Components/UnSignedOr/UnsignedOr.js
@@ -1,10 +1,28 @@
-import UnSigned from "../../Components/Unsigned/Unsigned.js";
-import Signed from "../Signed/Signed.js";
+import Signed from "../../Components/Signed/Signed.js";
+import Unsigned from "../../Components/Unsigned/Unsigned.js";
 function UnsignedOr({ num1, num2, setNum1, setNum2, calc, sign }) {
   if (sign === "unsigned") {
-    return UnSigned({ num1, num2, setNum1, setNum2, calc });
+    return (
+      <Unsigned
+        setNum1={setNum1}
+        setNum2={setNum2}
+        num1={num1}
+        num2={num2}
+        calc={calc}
+        sign={sign}
+      />
+    );
   } else if (sign === "signed") {
-    return Signed({ num1, num2, setNum1, setNum2, calc });
+    return (
+      <Signed
+        setNum1={setNum1}
+        setNum2={setNum2}
+        num1={num1}
+        num2={num2}
+        calc={calc}
+        sign={sign}
+      />
+    );
   }
 }
 
