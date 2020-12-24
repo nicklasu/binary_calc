6b06d9090bd1b8eff7f2decfc7170435d294fc97
nicklasu <nicklass84@gmail.com>
2020-12-24 14:51:49 +0200

    Now it displays stars correctly in addition

--- src/Functions/addStar.js
+++ src/Functions/addStar.js
@@ -5,9 +5,9 @@ function addStar(num1, num2, bitSize) {
   // Se jättää ton ekan tyhjäksi, sen takia ne tähdet menee vääriin paikkoihin.
   // Jos ottaa sen miinuksen pois bitSizestä se ei jätä ekaa arraytä tyhjäksi,
   // mutta esim 0110 + 0110 johtaa kolmeen *** vaikka pitäisi tulla vain kaksi.
-  const starArr = new Array(bitSize);
-  const bin1 = binaryToArray(num1);
-  const bin2 = binaryToArray(num2);
+  const starArr = new Array(bitSize).reverse();
+  const bin1 = binaryToArray(num1).reverse();
+  const bin2 = binaryToArray(num2).reverse();
 
   function carryToNext(numb1, numb2, numb3) {
     return (Number(numb1) + Number(numb2) + numb3) > 1;
@@ -22,8 +22,7 @@ function addStar(num1, num2, bitSize) {
       carry = 0;
     }
   }
-  console.log(starArr);
-  console.log(bin1);
+
   return starArr.join('').toString();
 }
 
