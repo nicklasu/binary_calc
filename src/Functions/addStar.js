ad7c71bc0803271df64a335de4f804cc6b856cc2
unknown <meteg66@gmail.com>
2020-12-26 20:54:02 +0200

    Stars working

--- src/Functions/addStar.js
+++ src/Functions/addStar.js
@@ -2,10 +2,9 @@ import binaryToArray from './Signed/BinaryToArray';
 
 function addStar(num1, num2, bitSize) {
   const starArr = [];
-  // Change binary strings to numbers
   const bin1 = binaryToArray(num1).map(Number).reverse();
   const bin2 = binaryToArray(num2).map(Number).reverse();
-  let carry;
+  let carry = false;
 
   for (let i = 0; i < bitSize; i += 1) {
     // Add star if 1 was carried
@@ -14,10 +13,6 @@ function addStar(num1, num2, bitSize) {
       ? '*'
       : '\u00A0';
 
-    // Check if 1 gets carried
-    // Evaluates to True or False
-    // True = 1
-    // False = 0
     carry = bin1[i] + bin2[i] + carry > 1;
   }
 
